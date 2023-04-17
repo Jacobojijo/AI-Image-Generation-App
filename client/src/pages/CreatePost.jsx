8cbf502823676f276934706ea41003fc1f739d63
Jacobojijo <jacobojio2017@gmail.com>
2023-04-17 10:59:45 +0300

    update ports

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,7 +19,7 @@ const CreatePost = () => {
       setloading(true)
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://backserver-77dj.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -42,7 +42,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setgeneratingImg(true)
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://backserver-77dj.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
